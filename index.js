c26650dc068daa6d944dd585e0b11680172a111c
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-24 22:16:58 +0600

    jwt done

--- index.js
+++ index.js
@@ -3,22 +3,37 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 // middle ware
 app.use(cors())
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kd8d4hj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 async function run(){
-    try{}
+    const userCollection = client.db('bookWorm').collection('users');
+    try{
+        app.post("/user",async (req,res)=>{
+            const user = req.body;
+            // console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, 
+                process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+    }
     catch{}
     finally{}
 }
