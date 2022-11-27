4b282f74988f35cda8d0064c50484fdbebdaf0bd
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-27 20:26:51 +0600

    payment api added

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const { query } = require('express');
 // middle ware
 app.use(cors())
 app.use(express.json());
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kd8d4hj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -132,6 +132,32 @@ async function run(){
             const result =await orderCollection.find({buyerEmail:email}).toArray();
             res.send(result)
         })
+        app.get('/order/:id', async(req,res)=>{
+            const id = req.params.id;
+            const result =await orderCollection.findOne({_id:ObjectId(id)});
+            res.send(result)
+        })
+        app.get('/book',async(req,res)=>{
+            const email = req.query.email;
+            const result =await bookCollection.find({buyerEmail:email}).toArray();
+            res.send(result)
+        })
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.product_price;
+            const amount = price * 100;
+            console.log(amount);
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
 }     
     
     catch{}
